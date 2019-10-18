7eac89c68e677705b453c3d3819508e62502e5e2
Ryan Smith <ryan.l.smith142@gmail.com>
2019-10-18 12:10:47 -0500

    complete exercises and bonus

--- src/main/java/GoodByeWorld.java
+++ src/main/java/GoodByeWorld.java
@@ -5,13 +5,15 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/hello-world")
-public class HelloWorldServlet extends HttpServlet {
+@WebServlet("/goodbye-world")
+public class GoodByeWorld extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
         res.setContentType("text/html");
         PrintWriter out = res.getWriter();
-        out.println("<h1>Hello, World!</h1>");
+        out.println("<h1>Goodbye, World!</h1>");
     }
+
+
 }
--- src/main/java/HelloNameServlet.java
+++ src/main/java/HelloNameServlet.java
@@ -5,13 +5,16 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/hello-world")
-public class HelloWorldServlet extends HttpServlet {
+@WebServlet("/hello-name")
+public class HelloNameServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        String name = req.getParameter("name");
         res.setContentType("text/html");
         PrintWriter out = res.getWriter();
-        out.println("<h1>Hello, World!</h1>");
+        out.println("<h1>Hello " + name + "</h1>");
     }
+
+
 }
