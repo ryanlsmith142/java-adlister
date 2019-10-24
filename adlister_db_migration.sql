3fd97911dfc9d5601e3b5819bdd030dcb9a9ed48
Ryan Smith <ryan.l.smith142@gmail.com>
2019-10-24 14:51:52 -0500

    add gitignore and database/tables

---
+++ adlister_db_migration.sql
@@ -0,0 +1,22 @@
+USE adlister_db;
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    username VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    PRIMARY KEY(id)
+)
+
+DROP TABLE IF EXISTS ads;
+
+CREATE TABLE ads(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    user_id INT UNSIGNED NOT NULL,
+    title VARCHAR(50) NOT NULL,
+    description VARCHAR(500) NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES users (id),
+    PRIMARY KEY(id)
+)
