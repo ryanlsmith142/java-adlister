3d9a27ea79dc39916aafb1fa87482f57184d4d54
Ryan Smith <ryan.l.smith142@gmail.com>
2019-10-28 07:10:35 -0500

    add user/login authentification

--- src/main/java/com/codeup/adlister/controllers/LoginServlet.java
+++ src/main/java/com/codeup/adlister/controllers/LoginServlet.java
@@ -1,6 +1,7 @@
 package com.codeup.adlister.controllers;
 
 import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.User;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -25,11 +26,15 @@ public class LoginServlet extends HttpServlet {
 
         // TODO: find a record in your database that matches the submitted password
 
-        DaoFactory.getUsersDao().findByUsername(username);
+        User loggedInUser = DaoFactory.getUsersDao().findByUsername(username);
         // TODO: make sure we find a user with that username
 
         // TODO: check the submitted password against what you have in your database
         boolean validAttempt = false;
+
+        if(password.equalsIgnoreCase(loggedInUser.getPassword())) {
+            validAttempt = true;
+        }
 
         if (validAttempt) {
             // TODO: store the logged in user object in the session, instead of just the username
